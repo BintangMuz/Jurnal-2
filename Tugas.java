639b9f11d87140c7b58bdc90d4f3b9c1bf0060b6
BintangMuz <71368686+BintangMuz@users.noreply.github.com>
2020-09-16 13:36:20 +0700

    Add files via upload

---
+++ Tugas.java
@@ -0,0 +1,10 @@
+public class Tugas  {
+
+   public static void main (String[]  args)  {
+	   System.out.println ("Nama :  Ilham Bintang Muzaki Sahid");
+	   System.out.println ("Nim  :  6706202077");
+ 	   System.out.println ("Kelas:  D3IF-44-02");
+	}
+}
+
+ 
